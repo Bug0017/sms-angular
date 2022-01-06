ef4df086e2ea61c97eb5b7f03157e5ae45c94fba
Surendar <bug0017@outlook.com>
2022-01-06 16:30:56 +0530

    removed nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule , NbSidebarModule, NbSidebarService} from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NbSidebarModule,
-    NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,
-    NbEvaIconsModule,
   ],
-  providers: [NbSidebarService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
